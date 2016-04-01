3de7f954332bd1bfee86e8ce8df7b487b80b34eb
Jesse Hockenbury <hockenburj1@nku.edu>
2016-04-01 16:04:20 -0400

    changed CSS of p with jQuery, removed empty paragraphs

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$( document ).ready( function() {
+    var paragraphs = $( 'p' );
+    var paragraphStyle =
+        {
+            'color': 'darkgray',
+            'line-height': '1.2',
+            'padding': '10px',
+            'background-color': '#f6f6f6'
+        };
+    paragraphs.css( paragraphStyle );
+} );
